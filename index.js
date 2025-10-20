15f9fa61ccaeb1a7f3d68d18d9567aff8f694d32
Sakey <sheikh36ahmed@gmail.com>
2025-10-20 11:15:16 +0100

    updates to render

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ const error = document.getElementById("error");
 
 let text = ""; // input value
 
-const createDetail = (name, type, content, url) => {
+const createDetail = (type, content, url) => {
+  if (!content) return;
   const element = document.createElement(type);
-  element.name = name;
   element.textContent = content || null;
   if (url) {
     if (type === "img") {
       element.src = url;
-      element.alt = "avatar";
-    } else if (type === "a") element.href = url;
+      element.alt = content;
+    } else if (type === "a") {
+      element.href = url;
+      element.id = "html_url";
+    }
   }
 
   profile.appendChild(element);
 };
 
 const createProfile = (details, avatar_url, html_url) => {
-  createDetail("avatar", "img", undefined, avatar_url); // avatar
+  createDetail("img", "avatar", avatar_url); // avatar
 
-  details.forEach((detail) => createDetail(detail.innerText, "p", detail)); // name, bio, etc
+  details.forEach((detail) => createDetail("p", detail)); // name, bio, etc
 
-  createDetail("profileLink", "a", "Visit Profile", html_url); // url link
+  createDetail("a", "Visit Profile", html_url); // url link
 };
 
 const handleSubmit = async () => {
@@ -48,7 +51,9 @@ const handleSubmit = async () => {
     const { avatar_url, name, login, location, bio, followers, following, public_repos, html_url } =
       await res.json();
 
-    const details = [name, login, location, bio, followers, following, public_repos];
+    const details = [name, login, location, bio, followers, following, public_repos]; // Attributes list
+
+    if (profile.children.length > 0) profile.innerHTML = ""; // Clear old profile before displaying new profile
 
     createProfile(details, avatar_url, html_url);
   } catch (err) {
@@ -57,28 +62,6 @@ const handleSubmit = async () => {
     submit.disabled = false;
   }
 };
-
-avatar_url: "https://avatars.githubusercontent.com/u/152389191?v=4";
-bio: "Not a vibe-coder";
-blog: "https://sheikh-ahm.netlify.app";
-company: null;
-followers: 1;
-followers_url: "https://api.github.com/users/sak3y/followers";
-following: 1;
-following_url: "https://api.github.com/users/sak3y/following{/other_user}";
-html_url: "https://github.com/sak3y";
-id: 152389191;
-location: "UK";
-login: "sak3y";
-name: "Sheikh";
-organizations_url: "https://api.github.com/users/sak3y/orgs";
-public_gists: 0;
-public_repos: 21;
-site_admin: false;
-starred_url: "https://api.github.com/users/sak3y/starred{/owner}{/repo}";
-type: "User";
-updated_at: "2025-10-02T22:37:20Z";
-user_view_type: "public";
 
 // listen for input
 input.addEventListener("keyup", (e) => {
